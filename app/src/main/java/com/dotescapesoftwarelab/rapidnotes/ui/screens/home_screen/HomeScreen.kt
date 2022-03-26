9f406eb4fbb583a24c1a2144562935566aef428e
Afjal Al Sayed <sayed01851@gmail.com>
2022-03-26 22:19:43 +0600

    home screen designed

--- app/src/main/java/com/dotescapesoftwarelab/rapidnotes/ui/screens/home_screen/HomeScreen.kt
+++ app/src/main/java/com/dotescapesoftwarelab/rapidnotes/ui/screens/home_screen/HomeScreen.kt
@@ -1,17 +1,19 @@
 package com.dotescapesoftwarelab.rapidnotes.ui.screens.home_screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.dotescapesoftwarelab.rapidnotes.ui.screens.home_screen.components.NoteItem
 
 @Composable
 fun HomeScreen(
@@ -23,14 +25,34 @@ fun HomeScreen(
 
     Scaffold(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxSize(),
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Icon(Icons.Default.Add, "Add new note")
+            }
+        }
     ) {
         LazyColumn(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxSize(),
+            contentPadding = PaddingValues(top = 24.dp, start = 16.dp, end = 16.dp, bottom = 96.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ){
+            item {
+                Text(
+                    text = "Rapid Notes",
+                    style = TextStyle(
+                        fontSize = 28.sp,
+                        fontWeight = FontWeight.Medium
+                    )
+                )
+            }
             items(notes){ note ->
-                Column{
+                NoteItem(note = note)
+                /*Column{
                     Text(
                         text = note.title
                     )
@@ -47,7 +69,7 @@ fun HomeScreen(
                         text = note.timestamp.toString()
                     )
                     Spacer(Modifier.height(8.dp))
-                }
+                }*/
             }
         }
     }
---
+++ app/src/main/java/com/dotescapesoftwarelab/rapidnotes/ui/screens/home_screen/components/NoteItem.kt
@@ -0,0 +1,88 @@
+package com.dotescapesoftwarelab.rapidnotes.ui.screens.home_screen.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dotescapesoftwarelab.rapidnotes.domain.model.Note
+
+@Composable
+fun NoteItem(
+    note: Note,
+    onDeleteClick: () -> Unit = {}
+) {
+
+    val cornerRadius = 8.dp
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(cornerRadius))
+            .background(Color(note.color))
+            .border(
+                width = 1.dp,
+                color = Color.Black,
+                shape = RoundedCornerShape(cornerRadius)
+            )
+            .clickable { }
+            .padding(16.dp)
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterStart)
+        ){
+            if(note.title.isNotBlank()){
+                Text(
+                    text = note.title,
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Medium
+                    ),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            if(note.title.isNotBlank() && note.content.isNotBlank()){
+                Spacer(Modifier.height(4.dp))
+            }
+            if(note.content.isNotBlank()) {
+                Text(
+                    text = note.content,
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    ),
+                    maxLines = 5,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
+        IconButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Delete Note"
+            )
+        }
+    }
+}
